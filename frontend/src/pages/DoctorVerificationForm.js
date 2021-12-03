46257498f108ca02cc1658bfd0d4e2425aa5f096
Kang-An <wkan2003@gmail.com>
2021-12-03 18:18:13 +0800

    Basic form validation for uploading of images done

--- frontend/src/pages/DoctorVerificationForm.js
+++ frontend/src/pages/DoctorVerificationForm.js
@@ -1,16 +1,58 @@
-import React from 'react';
-import { useForm } from "react-hook-form";
+import React, {useEffect, useState} from 'react';
+import { FormProvider, useForm } from "react-hook-form"
+import {useDropzone} from "react-dropzone";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const DoctorVerificationForm = () => {
-    const { register, errors, handleSubmit } = useForm();
+    const {register, unregister, setValue, formState: {errors}, handleSubmit} = useForm({mode: "onBlur"});
+    const [identificationImage, setIdentificationImage] = useState(null)
 
-    const onSubmit = async (data) => {
-        const fields = { fields: data };
-    };
+
+    const storage = getStorage();
+    const storageRef = ref(storage, 'images');
+
+    const idImageName = "idImage"
+
+    useEffect(() => {
+        register(idImageName, {required: true});
+        return () => {
+            unregister(idImageName);
+        };
+    }, []);
+
+
+    const {acceptedFiles, fileRejections, getRootProps, getInputProps} = useDropzone({
+        maxFiles: 1,
+        accept: 'image/*',
+        onDrop: acceptedFiles => {
+            console.log("ACCEPTED FILES ")
+            const file = acceptedFiles[0]
+            if (file) {
+                console.log(URL.createObjectURL(file))
+                setIdentificationImage(URL.createObjectURL(file))
+                setValue(idImageName, file, { shouldValidate: true });
+                // uploadBytes(storageRef, file).then((snapshot) => {
+                //     console.log(snapshot.ref.fullPath)
+                //     console.log('Uploaded a blob or file!');
+                // });
+            }
+        }
+    })
+
+    // useEffect(() => () => {
+    //     console.log("REVOKING")
+    //     // Make sure to revoke the data uris to avoid memory leaks
+    //     URL.revokeObjectURL(identificationImage)
+    // }, [identificationImage]);
+
+
+    function onSubmit(data) {
+        console.log(data);
+    }
 
     return (
-        <div className="mt-5 md:mt-0">
-            <form action="#" method="POST">
+        <div className="max-w-7xl mt-5 md:mt-0">
+            <form onSubmit={handleSubmit(onSubmit)} method="POST">
                 <div className="shadow overflow-hidden sm:rounded-md">
                     <div className="px-4 py-5 bg-white sm:p-6">
                         <div className="grid grid-cols-6 gap-6">
@@ -19,12 +61,15 @@ const DoctorVerificationForm = () => {
                                     First name
                                 </label>
                                 <input
+
                                     type="text"
                                     name="first-name"
                                     id="first-name"
                                     autoComplete="given-name"
                                     className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    {...register("firstName", {required: true, maxLength: 2})}
                                 />
+                                {errors.firstName && <p>First name is required</p>}
                             </div>
 
                             <div className="col-span-6 sm:col-span-3">
@@ -39,6 +84,47 @@ const DoctorVerificationForm = () => {
                                     className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                             </div>
+
+                            <div className="col-span-6 sm:col-span-4">
+                                <label className="block text-sm font-medium text-gray-700">Identification</label>
+                                <div {...getRootProps({className: "mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"})} >
+                                    {/*<div {...getRootProps()} className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">*/}
+                                    <div className="space-y-1 text-center">
+                                        <svg
+                                            className="mx-auto h-12 w-12 text-gray-400"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            viewBox="0 0 48 48"
+                                            aria-hidden="true"
+                                        >
+                                            <path
+                                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                                strokeWidth={2}
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                        <div className="flex text-sm text-gray-600">
+                                            <label
+                                                htmlFor="file-upload"
+                                                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                                            >
+                                                <span>Upload a file</span>
+                                                {/*<input {...getInputProps()} id="file-upload" name="file-upload" type="file" className="sr-only" />*/}
+                                                <input  id="file-upload" name="idImageName"
+                                                       type="file" className="sr-only" {...getInputProps()} />
+                                            </label>
+                                            <p className="pl-1">or drag and drop</p>
+                                        </div>
+                                        <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                                    </div>
+                                </div>
+                                {errors.idImage && <p>ID IMAGE MISSING</p>}
+
+                            </div>
+
+
+
 
                             <div className="col-span-6 sm:col-span-4">
                                 <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
